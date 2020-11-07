47be9b61a3e117a6e968af40f994f3c440ae0b4f
takumi-takahashi-0806 <coyote0039@gmail.com>
2020-11-07 19:39:41 +0900

    実装完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+def new
+  @item = Item.new
 end
+
+def create
+  @item = Item.create(item_params)
+  if @item.save
+    redirect_to root_path
+  else
+    render :new
+  end
+end
+
+
+private
+def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :first_name, :last_name, :kanafirst_name, :kanamiddle_name, :birthday])
+end
+end
+
---
+++ app/models/user.rb
@@ -0,0 +1,36 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         # ニックネームが必須であること
+         validates :nickname, presence: true
+
+         # パスワードは、半角英数字混合での入力が必須であること
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX 
+
+         #パスワードは、確認用を含めて2回入力すること,パスワードとパスワード（確認用）、値の一致が必須であること
+         validates :password, confirmation: true
+         validates :password_confirmation, presence: true
+
+
+         #全角（漢字・ひらがな・カタカナ）での入力が必須であること
+        with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/} do
+          validates :first_name
+          validates :last_name
+        end
+         
+         #全角（カタカナ）での入力が必須であること
+        with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/} do
+          validates :kanafirst_name
+          validates :kanamiddle_name
+        end
+
+        #生年月日が必須であること
+         validates :birthday, presence: true
+
+         has_many :items
+         has_many :buy_items
+end
