98e73d4c59b60e064b36e76b01d0d43885432a28
takumi-takahashi-0806 <coyote0039@gmail.com>
2020-11-14 21:14:27 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,29 @@
 class ItemsController < ApplicationController
-  #def new
-    #@item = Item.new
+  before_action :authenticate_user!, only: [:new]
+
+  def index
+    @items = Item.all
+    @items = Item.includes(:user).order("created_at DESC")
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  #def show
+    #@item = Item.find(params[:id])
   #end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :product_details_id, :delivery_fee_id, :shipping_area_id, :shipping_days_id, :price, :image).merge(user_id: current_user.id)
+  end
   
   #def create
     #@item = Item.create(item_params)
