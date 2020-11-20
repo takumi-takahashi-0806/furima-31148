03c6438c1027033ecba8ad97fa8145d2995ce3d9
takumi-takahashi-0806 <coyote0039@gmail.com>
2020-11-20 14:08:40 +0900

    コミット

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
 
 
@@ -7,5 +8,11 @@ private
 def configure_permitted_parameters
   devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :first_name, :last_name, :kanafirst_name, :kanamiddle_name, :birthday])
 end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'takahashi' && password == 'ifc12345'
+    end
+  end
 end
 
